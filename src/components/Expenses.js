3e2980bbcef48fc823cf8140d94046e41547767f
Tarmo Liivamägi <liivamagi.tarmo@gmail.com>
2023-05-18 18:21:54 +0300

    after app.js change

---
+++ src/components/Expenses.js
@@ -0,0 +1,42 @@
+import ExpenseItem from './ExpenseItem';
+import './Expenses.css';
+
+function item () {
+    const items = [
+        {
+          id: 'e1',
+          title: 'Toilet Paper',
+          amount: 94.12,
+          date: new Date(2020, 7, 14),
+        },
+        { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+        {
+          id: 'e3',
+          title: 'Car Insurance',
+          amount: 294.67,
+          date: new Date(2021, 2, 28),
+        },
+        {
+          id: 'e4',
+          title: 'New Desk (Wooden)',
+          amount: 450,
+          date: new Date(2023, 5, 12),
+        },
+      ];
+
+
+    return(
+        <div className="expenses">
+            {items.map((item) => (
+                <ExpenseItem
+                    key={item.id}
+                    title={item.title}
+                    amount={item.amount}
+                    date={item.date}
+                />
+            ))}
+        </div>
+    )
+}
+
+export default item;
