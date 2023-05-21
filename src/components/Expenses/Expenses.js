ae5c7205fd198498cf5f9ce846a812284f020e20
Tarmo Liivamägi <liivamagi.tarmo@gmail.com>
2023-05-21 17:42:49 +0300

    bug fix

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,33 +2,11 @@ import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 import Card from '../UI/Card';
 
-function item () {
-    const items = [
-        {
-          id: 'e1',
-          title: 'Toilet Paper',
-          amount: 94.12,
-          date: new Date(2020, 7, 14),
-        },
-        { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-        {
-          id: 'e3',
-          title: 'Car Insurance',
-          amount: 294.67,
-          date: new Date(2021, 2, 28),
-        },
-        {
-          id: 'e4',
-          title: 'New Desk (Wooden)',
-          amount: 450,
-          date: new Date(2023, 5, 12),
-        },
-      ];
-
+function Expenses (props) {
 
     return(
         <Card className="expenses">
-            {items.map((item) => (
+            {props.items.map((item) => (
                 <ExpenseItem
                     key={item.id}
                     title={item.title}
@@ -40,4 +18,4 @@ function item () {
     )
 }
 
-export default item;
+export default Expenses;
