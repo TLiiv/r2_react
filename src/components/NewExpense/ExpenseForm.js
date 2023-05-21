3e772ca8ff1c8ff038027accf686e3b723f68752
Tarmo Liivamägi <liivamagi.tarmo@gmail.com>
2023-05-21 17:35:30 +0300

    some data upflow

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,12 +1,20 @@
 import React, { useState } from 'react';
 import './ExpenseForm.css';
 
-function ExpenseForm(){
+function ExpenseForm(props){
     const [enteredTitle, setEnteredTitle] = useState("");
     const [enteredAmount, setEnteredAmount] = useState('');
     const [enteredDate, setEnteredDate] = useState("");
-   
+//   const [userInput,setUserInput] = useState({
+//     enteredTitle:"",
+//     enteredAmount:'',
+//     enteredDate:''
+//    })
+
     const titleChangeHandler = (e) => {
+        // setUserInput((prevState)=>{
+        //     return {...prevState,enteredTitle:e.target.value}
+        // }) //when using one useState
         setEnteredTitle(e.target.value);
     };
     const amountChangeHandler = (e) => {
@@ -15,11 +23,22 @@ function ExpenseForm(){
     const dateChangeHandler = (e) => {
         setEnteredDate(e.target.value);
     };
-
+    const submitHandler = (e) => {
+        e.preventDefault();
+        const expenseData = {
+          title: enteredTitle,
+          amount:enteredAmount,
+          date: new Date(enteredDate)
+        };
+        props.onSaveExpenseData(expenseData);
+        setEnteredTitle("");
+        setEnteredAmount("");
+        setEnteredDate("");
+    }
 
 
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
