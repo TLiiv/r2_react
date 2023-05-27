123494df2bc03d99b28c0f91536c73d926599051
Tarmo Liivamägi <liivamagi.tarmo@gmail.com>
2023-05-27 13:53:33 +0300

    finished ChartBar

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 
 import './Expenses.css';
 
@@ -28,6 +29,7 @@ function Expenses(props) {
                     onFilterChange={expensesFilterHandler}
                     selected={filteredYear}
                 />
+                <ExpensesChart expenses={filteredExpenses} />
                 <ExpensesList filteredExpenses={filteredExpenses} />
 
             </Card>
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -27,7 +27,7 @@ function ExpenseForm(props) {
         e.preventDefault();
         const expenseData = {
             title: enteredTitle,
-            amount: enteredAmount,
+            amount: +enteredAmount,
             date: new Date(enteredDate)
         };
         props.onSaveExpenseData(expenseData);
